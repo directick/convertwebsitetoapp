c832d890945a7489e93121f45fc7eee453c0389f
directick <96158176+directick@users.noreply.github.com>
2021-12-15 10:56:38 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'com.directick',
+  appName: 'directick.com',
   webDir: 'www',
   bundledWebRuntime: false,
 };
